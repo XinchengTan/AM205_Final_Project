ad6e89791be943628c42e0c460c8f72a947b884e
sunny-lei <sunny_lei@berkeley.edu>
2020-12-17 12:34:27 -0800

    refresh RLA.py

--- RLA.py
+++ RLA.py
@@ -11,118 +11,119 @@ from sklearn import linear_model
 from scipy.optimize import minimize
 
 def RLA_mult(A, B, c, distribution = "uniform"):
-	"""
-	This function takes two large matrices A and B,
-	and computes A*B through random linear algebra method.
+    """
+    This function takes two large matrices A and B,
+    and computes A*B through random linear algebra method.
 
-	input:
-	A: m * n matrix (it can be numpy array or 2D python list)
-	B: n * p matrix (it can be numpy array or 2D python list)
-	c: positive integer, the number of samples
-	distribution: takes two values ["uniform", "minvar"]
-				  "minvar" is a probability distribution minimizes variance.
-	----------------
-	output: approximation for AB
-	"""
+    input:
+    A: m * n matrix (it can be numpy array or 2D python list)
+    B: n * p matrix (it can be numpy array or 2D python list)
+    c: positive integer, the number of samples
+    distribution: takes two values ["uniform", "minvar"]
+                  "minvar" is a probability distribution minimizes variance.
+    ----------------
+    output: approximation for AB
+    """
 
-	# convert A and B to numpy arrays
-	if type(A) is list:
-		A = np.array(A)
-	if type(B) is list:
-		B = np.array(B)
-
-
-	m = A.shape[0]    # A: m*n; B:n*p
-	n = A.shape[1]
-	p = B.shape[1]
-	# check if dimensions of two matrices match
-	if n != B.shape[0]:
-		print("Dimensions of two matrices don't match.")
-		return
+    # convert A and B to numpy arrays
+    if type(A) is list:
+        A = np.array(A)
+    if type(B) is list:
+        B = np.array(B)
 
 
-	# if the selected sampling distribution is uniform
-	if distribution == "uniform":
-		answer = np.zeros((m, p))
-		for t in range(c):
-			i = random.randint(0, n-1)
-			A_icol = A[:, [i]]
-			B_irow = B[[i], :]
-			answer += np.matmul(A_icol, B_irow)
-		return n * answer / c
+    m = A.shape[0]    # A: m*n; B:n*p
+    n = A.shape[1]
+    p = B.shape[1]
+    # check if dimensions of two matrices match
+    if n != B.shape[0]:
+        print("Dimensions of two matrices don't match.")
+        return
 
 
-	# if the selected sampling distribution is to minimize variance
-	elif distribution == "minvar":
-		# calculate the sample distribution
-		weight_sum = 0
-		weights = []
-		for i in range(n):
-			w = np.linalg.norm(A[:, i]) * np.linalg.norm(B[i, :])
-			weights.append(w)
-			weight_sum += w
-		weights = np.array(weights) / weight_sum
+    # if the selected sampling distribution is uniform
+    if distribution == "uniform":
+        answer = np.zeros((m, p))
+        for t in range(c):
+            i = random.randint(0, n-1)
+            A_icol = A[:, [i]]
+            B_irow = B[[i], :]
+            answer += np.matmul(A_icol, B_irow)
+        return n * answer / c
 
-		# sample and calculate the answer
-		answer = np.zeros((m, p))
-		for t in range(c):
-			i = np.random.choice(np.arange(0, n), p = weights)
-			A_icol = A[:, [i]]
-			B_irow = B[[i], :]
-			answer += np.matmul(A_icol, B_irow) * (1/weights[i])
-		return answer / c
 
-	else:
-		print("Probability distribution type not valid.")
+    # if the selected sampling distribution is to minimize variance
+    elif distribution == "minvar":
+        # calculate the sample distribution
+        weight_sum = 0
+        weights = []
+        for i in range(n):
+            w = np.linalg.norm(A[:, i]) * np.linalg.norm(B[i, :])
+            weights.append(w)
+            weight_sum += w
+        weights = np.array(weights) / weight_sum
+
+        # sample and calculate the answer
+        answer = np.zeros((m, p))
+        for t in range(c):
+            i = np.random.choice(np.arange(0, n), p = weights)
+            A_icol = A[:, [i]]
+            B_irow = B[[i], :]
+            answer += np.matmul(A_icol, B_irow) * (1/weights[i])
+        return answer / c
+
+    else:
+        print("Probability distribution type not valid.")
+
 
 
 
 
 
 def generate_matrix(m, n, method=1, param = [0,1]):
-	"""
-	This function takes in dimensions m and n,
-	to randomly generate a matrix of m*n.
-	Each element in the matrix is drawn according to the method and param specified.
+    """
+    This function takes in dimensions m and n,
+    to randomly generate a matrix of m*n.
+    Each element in the matrix is drawn according to the method and param specified.
 
-	input:
-	m: int; the number of rows
-	n: int; the number of columns
-	method: int; represent the method/distirbution we generate matrix from
-	        method = 1: all elements drawn from Uniform(param[0], param[1]).
-	        method = 2: all elements drawn from Normal(param[0], param[1]^2)
-			method = 3: all elements are binary, take either 0 (probability of param[0]) or 1 (probability of param[1]),
-						where param[0]+param[1] = 1.
-	----------------
-	output: a 2-D numpy array representing the randomly generated matrix according to method and param.
-	"""
-	# uniform distribution
-	if method == 1:
-		M = np.random.rand(m,n)
-		if len(param) != 2:
-			return M
-		M = M * (param[1]-param[0])
-		M = M + param[0]
-		return M
+    input:
+    m: int; the number of rows
+    n: int; the number of columns
+    method: int; represent the method/distirbution we generate matrix from
+            method = 1: all elements drawn from Uniform(param[0], param[1]).
+            method = 2: all elements drawn from Normal(param[0], param[1]^2)
+            method = 3: all elements are binary, take either 0 (probability of param[0]) or 1 (probability of param[1]),
+                        where param[0]+param[1] = 1.
+    ----------------
+    output: a 2-D numpy array representing the randomly generated matrix according to method and param.
+    """
+    # uniform distribution
+    if method == 1:
+        M = np.random.rand(m,n)
+        if len(param) != 2:
+            return M
+        M = M * (param[1]-param[0])
+        M = M + param[0]
+        return M
 
-	# normal distribution
-	if method == 2:
-		if len(param) != 2:
-			return np.random.normal(0, 1, size=(m, n))
-		else:
-			return np.random.normal(param[0], param[1], size=(m, n))
+    # normal distribution
+    if method == 2:
+        if len(param) != 2:
+            return np.random.normal(0, 1, size=(m, n))
+        else:
+            return np.random.normal(param[0], param[1], size=(m, n))
 
-	# bernoumlli distribution
-	if method == 3:
-		M = np.zeros((m,n))
-		for i in range(m):
-			for j in range(n):
-				elem = np.random.choice(np.arange(0, 2), p = param)
-				M[i,j] = elem
-		return M
+    # bernoumlli distribution
+    if method == 3:
+        M = np.zeros((m,n))
+        for i in range(m):
+            for j in range(n):
+                elem = np.random.choice(np.arange(0, 2), p = param)
+                M[i,j] = elem
+        return M
 
-	else:
-		print("Specified method not recognized.")
+    else:
+        print("Specified method not recognized.")
 
 
 
@@ -130,22 +131,22 @@ def generate_matrix(m, n, method=1, param = [0,1]):
 
 
 def frobenius_dist(true_mat, approx_mat):
-	"""
-	This function computes the frobenius distance between the approximated matrix
-	and the true matrix.
-	Which is given by the frobenius norm of (true_mat-approx_mat)/
+    """
+    This function computes the frobenius distance between the approximated matrix
+    and the true matrix.
+    Which is given by the frobenius norm of (true_mat-approx_mat)/
 
-	input:
-	true_mat: numpy 2-D array; the true matrix
-	approx_mat: numpy 2-D array; the approximated matrix
-	----------------
-	output: double; the frobenius distance between true_mat and approx_mat
-	"""
-	if true_mat.shape[0] == approx_mat.shape[0] and true_mat.shape[1] == approx_mat.shape[1]:
-		diff = true_mat - approx_mat
-		return LA.norm(diff, 'fro')
-	else:
-		print("Dimensions of two matricies doesn't match.")
+    input:
+    true_mat: numpy 2-D array; the true matrix
+    approx_mat: numpy 2-D array; the approximated matrix
+    ----------------
+    output: double; the frobenius distance between true_mat and approx_mat
+    """
+    if true_mat.shape[0] == approx_mat.shape[0] and true_mat.shape[1] == approx_mat.shape[1]:
+        diff = true_mat - approx_mat
+        return LA.norm(diff, 'fro')
+    else:
+        print("Dimensions of two matricies doesn't match.")
 
 
 
@@ -187,7 +188,6 @@ def traditional_mult(A, B):
 
 
 
-
 def RLasso_sampling(A, b, err_tolerance, alpha=0.1):
     """
     A is a n*d matrix, b is a n-element vector.
@@ -207,9 +207,8 @@ def RLasso_sampling(A, b, err_tolerance, alpha=0.1):
 
     # compute the proper r
     ln40nd = np.log(40*n*d)
-    r = max(48*48*d*ln40nd*np.log(10000*d*ln40nd), 40*d*ln40nd/err_tolerance)
+    r = min(max(48*48*d*ln40nd*np.log(10000*d*ln40nd), 40*d*ln40nd/err_tolerance), 5*n)
     r = int(r)
-    print("Computed r is", r)
     
     # construct S matrix
     S = np.zeros((n,r))
@@ -218,7 +217,12 @@ def RLasso_sampling(A, b, err_tolerance, alpha=0.1):
         S[k_l, l] = math.sqrt(n/r)
     
     # construct H, note that H is n*n where n is a power of 2
-    H = hadamard(n)/sqrt(n)
+    # find the smallest power of 2 n' greater or equal to n 
+    power =  ceil(np.log2(n))
+    n_ = pow(2, power) 
+    H = hadamard(n_)/sqrt(n_)
+    # truncate H to only have n*n entries
+    H = H[0:n,0:n]
     
     # construct D, note that D is n*n diagonal matrix
     D = np.identity(n)
@@ -237,7 +241,8 @@ def RLasso_sampling(A, b, err_tolerance, alpha=0.1):
     # returns a model object.
     # use clf.intercept_ to get the intercept
     # use clf.coef_ to get the coefficients
-    return clf
+    return clf.coef_
+    # return np.append(clf.coef_, clf.intercept_)
 
 
 
@@ -245,9 +250,7 @@ def RLasso_sampling(A, b, err_tolerance, alpha=0.1):
 def RLasso_projection(A, b, err_tolerance, Cq, Ck, alpha=0.1):
     """
     A is a n*d matrix, b is a n-element vector.
-    We aim to use random sampling to construct a reduced version of the problem,
-    and solve through LASSO regression.
-    
+    We aim to use random sampling to construct a reduced version of the problem, and solve through LASSO regression.
     input: 
     A: numpy 2D array of n*d representing data points where n is the number of data points
     b: n-element vector
@@ -264,7 +267,7 @@ def RLasso_projection(A, b, err_tolerance, Cq, Ck, alpha=0.1):
     # compute the proper r
     ln40nd = np.log(40*n*d)
     q = Cq*d*ln40nd/n*(2*np.log(n)+16*d+16)
-    print(q)
+    print("here")
     r = int(max(Ck*(188*188*d+98*98), 60*d/err_tolerance))
     
     # construct T matrix
@@ -324,7 +327,7 @@ def RLA_cov(X, c, assume_centered=True, sampler="uniform", bias=False):
 
 
 
-def SLRviaRP(X, y, lam_0, lam_min, k, eta, T, gamma=0):
+def SLRviaRP(X, y, lam_0, lam_min, k, eta, T, gamma=0.01):
 	"""
 	Accelerated Sparse Linear Regression via Random Projection.
 
@@ -351,7 +354,7 @@ def SLRviaRP(X, y, lam_0, lam_min, k, eta, T, gamma=0):
 		# now we solve the optimization problem listed in the algorithm line 9
 
 		B_next = minimize(f, B_t, method="nelder-mead").x
-
+		print(B_next)
 		B_t = B_next
 
 	return B_t
