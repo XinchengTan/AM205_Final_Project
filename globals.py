9e2b24d739f5688acef717a9417cd6bf8c1e5349
Xincheng Tan <caratan1998@yahoo.com>
2020-12-26 23:35:47 -0500

    minor edits

--- globals.py
+++ globals.py
@@ -10,31 +10,6 @@ SEED = 21000
 SESSION_NAME = 0
 REC_DATE = 2
 
-# Stimuli type
-SPONT = "spont"
-VISUAL = "visual"
-GRATING = "grating"
-
-# Inter-neuron distance types
-DIST_TYPES = ["euclidean", "minkowski", "sqeuclidean", "cosine"]
-EUCLIDEAN = 0
-MINKOWSKI = 1
-SQEUCLIDEAN = 2
-COSINE = 3
-
 # PSD Method
 NEWTON = "newton"
 ALT_PROJ = "alt_proj"
-
-# Similarity metrics
-SIM_METRICS = []
-
-# Estimator types
-EMPIRICAL = "Empirical"
-GLASSO = "Glasso"
-AGLASSO = "AGlasso"
-BAGLASSO = "BAGlasso"
-SAGLASSO = "SAGlasso"
-GAR = "Gar"
-
-
